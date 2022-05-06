3967c3116c1bd293e9a67e75b988804b422e39e9
camilord-end <juliancaor10@gmail.com>
2022-05-06 18:02:41 -0500

    challenge 1 done

---
+++ challenge1/index.js
@@ -0,0 +1,21 @@
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'},
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+const contarOvejas = (ovejas) => {
+  const ovejasFiltered = ovejas.filter((c)=>{
+    const containN = c.name.toLowerCase().includes('n')
+    const containA = c.name.toLowerCase().includes('a')
+    if (c.color==='rojo' && containN && containA) { return true }
+  })
+  return ovejasFiltered
+}
+
+const ovejasFiltradas = contarOvejas(ovejas)
+
+console.log(ovejasFiltradas)
